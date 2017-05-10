bc5103ed9c6c8e2d919e4e479107eec6043df5ef
Andrea Kanya <andrea.kanya@gmail.com>
2017-05-10 19:55:39 +0200

    files added

--- exercise_3_refactor2.py
+++ exercise_3_refactor2.py
@@ -5,22 +5,28 @@ import math
 def get_user_input(range_min, range_max):
     numbers = []
     for item in range(range_min, range_max):
-        user_input = int((input("Please enter a number")))
+        user_input = int((input("Please enter a number: ")))
         numbers.append(user_input)
     return numbers
 
 
-def main():
-    a = get_user_input(0, 12)
+def operations(numbers_list):
     count = 0
-    half_item = len(a)/2
+    half_item = len(numbers_list)/2
     for item in range(round(half_item)):
-        print(" ")
-        print(str(a[count+1]/a[count]) + "\n")
+        print("-" * count)
+        print(str(numbers_list[count+1]/numbers_list[count]) + "\n")
         count += 1
-    for item in range(len(a)-round(a)):
-        print("      ")
-        print(str(a[count+1]*a[count]) + "\n")
+    for item in range(round(half_item), len(numbers_list)):
+        print("-" * (len(numbers_list) - count))
+        print(str(numbers_list[count+1]*numbers_list[count]) + "\n")
+        count += 1
+
+
+def main():
+    user_input_numbers = get_user_input(0, 12)
+    operations(user_input_numbers)
+
 
 if __name__ == '__main__':
     main()
